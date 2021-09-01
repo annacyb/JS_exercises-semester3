7dd03ffc5ccb315e30978335bcce104c3cff4a33
AnnaCybulska <anna357y@stud.kea.dk>
2021-09-01 13:37:57 +0200

    TypeWriter exercise done

--- 11C_02_02-TypeWriter/typewriter.js
+++ 11C_02_02-TypeWriter/typewriter.js
@@ -1,1 +1,37 @@
 "use strict";
+
+let startValue = document.querySelector(".typewritten").textContent
+document.querySelector(".typewritten").textContent = ""
+
+let splittedSentence = startValue.split("")
+console.log(splittedSentence)
+
+let iterator
+let maxNumber
+let a
+init()
+
+function init() {
+    iterator = -1
+    maxNumber = startValue.length
+    console.log(maxNumber)
+    loop()
+}
+
+function loop() {
+    iterator++
+    a = splittedSentence[iterator]
+    document.querySelector(".typewritten").textContent += a
+    // console.log(a)
+    if(iterator < maxNumber) {
+        setTimeout(loop, 500)
+    }
+}
+
+
+
+
+
+
+
+
