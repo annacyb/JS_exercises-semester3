c58ab5e78829d4f3a010b050765cdceed3626f0d
AnnaCybulska <anna357y@stud.kea.dk>
2021-09-01 18:35:26 +0200

    CountingArray exercise finished

---
+++ 11C_02_03-CountingArray/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+
+// Every second, the counter increments by one. The counter is then added
+// to the beginning of an array. The array is “truncated” so that it never
+// becomes longer than 9.
+
+// It should look like that:
+// [0]
+// [1,0]
+// [2,1,0]
+// [3,2,1,0]
+// ...
+// [18,17,16,15,14,13,12,11,10] - max 9 elements in the array
+// [19,18,17,16,15,14,13,12,11] 
+
+let counter = -1
+let array1 = []
+init()
+
+function init() {
+    counting()
+}
+
+function counting() {
+    counter++
+    if (counter < 100) {
+        if (array1.length < 9) {
+            array1.unshift(counter)
+            console.log(array1)
+            setTimeout(init, 1000)
+        }
+        else {
+            array1.pop()
+            array1.unshift(counter)
+            console.log(array1)
+            setTimeout(init, 1000)
+        }
+    }
+}
+
+
+
+
+
+
