cec84aecd9746c7afca6301de8bb8befebf9c773
AnnaCybulska <anna357y@stud.kea.dk>
2021-09-05 16:08:13 +0200

    Finished Animal Base

--- FirstDatabaseExercise-Animal_base/animals_objects.js
+++ FirstDatabaseExercise-Animal_base/animals_objects.js
@@ -1,13 +1,21 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", start);
+window.addEventListener("DOMContentLoaded", start)
 
-const allAnimals = [];
+const allAnimals = []
+
+// creating object prototype
+const Animal = {
+    name: "",
+    type: "unknown",
+    desc: "",
+    age: 0
+}
 
 function start( ) {
-    console.log("ready");
+    console.log("ready")
 
-    loadJSON();
+    loadJSON()
 }
 
 
@@ -16,40 +24,50 @@ function loadJSON() {
     .then( response => response.json() )
     .then( jsonData => {
         // when loaded, prepare objects
-        prepareObjects( jsonData );
+        prepareObjects( jsonData )
     });
 }
 
 function prepareObjects( jsonData ) {
     jsonData.forEach( jsonObject => {
-        // TODO: Create new object with cleaned data - and store that in the allAnimals array
         
-        // TODO: MISSING CODE HERE !!!
-    });
+        // creating an object from the prototype
+        const animal = Object.create(Animal)
+        // editing prototype
+        let nameSplit = jsonObject.fullname.split(" ")
+        animal.name = nameSplit[0]
+        animal.type = nameSplit[nameSplit.length - 1]
+        animal.desc = nameSplit[2]
+        animal.age = jsonObject.age
 
-    displayList();
+        allAnimals.push(animal)
+
+    })
+    console.log(allAnimals)
+    displayList()
 }
+
 
 function displayList() {
     // clear the list
-    document.querySelector("#list tbody").innerHTML = "";
+    document.querySelector("#list tbody").innerHTML = ""
 
     // build a new list
-    allAnimals.forEach( displayAnimal );
+    allAnimals.forEach( displayAnimal )
 }
 
 function displayAnimal( animal ) {
     // create clone
-    const clone = document.querySelector("template#animal").content.cloneNode(true);
+    const clone = document.querySelector("template#animal").content.cloneNode(true)
 
     // set clone data
-    clone.querySelector("[data-field=name]").textContent = animal.name;
-    clone.querySelector("[data-field=desc]").textContent = animal.desc;
-    clone.querySelector("[data-field=type]").textContent = animal.type;
-    clone.querySelector("[data-field=age]").textContent = animal.age;
+    clone.querySelector("[data-field=name]").textContent = animal.name
+    clone.querySelector("[data-field=desc]").textContent = animal.desc
+    clone.querySelector("[data-field=type]").textContent = animal.type
+    clone.querySelector("[data-field=age]").textContent = animal.age
 
     // append clone to list
-    document.querySelector("#list tbody").appendChild( clone );
+    document.querySelector("#list tbody").appendChild( clone )
 }
 
 
