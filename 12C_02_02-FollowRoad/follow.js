7a33f6ada950b9f2a6344df8a02dbf36a191ce74
AnnaCybulska <anna357y@stud.kea.dk>
2021-10-04 21:06:33 +0200

    Follow the road exercise done

--- 12C_02_02-FollowRoad/follow.js
+++ 12C_02_02-FollowRoad/follow.js
@@ -1,26 +1,26 @@
-window.addEventListener("DOMContentLoaded", loadSVG);
+"use strict"
+
+window.addEventListener("DOMContentLoaded", loadSVG)
 
 function loadSVG() {
-    console.log("load the SVG");
+    console.log("load the SVG")
 
     fetch("car_and_road.svg")
     .then( response => response.text() )
     .then( svgData => {
-        console.log("SVG loaded");
-
-        // TODO: put the SVG into the DOM
-
-        // TODO: Start the animation
+        console.log("SVG loaded")
+        document.querySelector("#thesvg").innerHTML = svgData
+        runAnimation()
 
     })
 }
 
-let car = null;
-let curve = null;
-let currentPosition;
+let car = null
+let curve = null
+let currentPosition
 
 function runAnimation() {
-    console.log("animate");
-
-    // TODO: Build animation ...
+    console.log("animate")
+    const car = document.querySelector("#car")
+    car.classList.add("moveOnPath")
 }
