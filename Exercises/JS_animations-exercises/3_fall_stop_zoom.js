e2e5cc2d3a1fbd0de6429b6c80ab6b363f14d4af
AnnaCybulska <anna357y@stud.kea.dk>
2021-10-05 23:18:11 +0200

    Added 2 animations by JS to HTML element

--- Exercises/JS_animations-exercises/3_fall_stop_zoom.js
+++ Exercises/JS_animations-exercises/3_fall_stop_zoom.js
@@ -2,16 +2,53 @@
 
 const ball = document.querySelector("#ball");
 
-// Create keyframes and properties for falling and zoom
+// 1. Create keyframes and properties for falling and zoom
 
-// register click
-
-// start falling animation
-
-function ballClicked() {
-  console.log("Ball clicked!");
-  // pause falling animation
-
-  // start zoom-animation
-
+// falling animation
+const propertiesFall = {
+  duration: 1000,
+  iterations: Infinity,
+  // direction: "alternate",
+  easing: "ease-in"
 }
+
+const keyframesFall = [
+  { transform: `translateY(-20vw)`},
+  { transform: `translateY(65vw)`}
+]
+
+// zoom animation
+const propertiesZoom = {
+  duration: 500,
+  iterations: 1,
+  // direction: "alternate",
+  easing: "ease-in-out",
+  fill: "forwards",
+  composite: "add"
+}
+
+const keyframesZoom = [
+  { transform: `scale(1)`},
+  { transform: `scale(0)`}
+]
+
+// 2. Start falling animation
+const animation = ball.animate( keyframesFall, propertiesFall )
+
+// 3. Register click
+ball.addEventListener("click", () => {
+  console.log("Ball clicked!")
+  console.log(ball)
+  // pause falling animation
+  animation.pause()
+  // start zoom-animation
+  const animationZoom = ball.animate( keyframesZoom, propertiesZoom )
+})
+
+
+
+
+
+
+
+
